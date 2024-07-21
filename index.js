937e640943a715e018992c484a461f603031665e
Klinsmann Ouma <oumamalaki@gmail.com>
2024-07-21 20:18:45 +0300

    third commit. Carousel and FAQ section pending

--- index.js
+++ index.js
@@ -1,32 +1,59 @@
-const burger = document.querySelector('#burger');
-const menu = document.querySelector('#menu');
+const questions = document.querySelectorAll('#quiz')    
+;
 
-const containers = document.querySelector('#containers');
-const contList = document.querySelector('contList');
+//Menu on click action
+function openClose() {
+   const menu = document.querySelector('#menu');
 
-const chevron = document.querySelector('#chevron');
-const response = document.querySelector('#response');
+   if (menu.classList.contains('hidden'))
+   {
+      menu.classList.remove('hidden');
+   } else {
+      menu.classList.add('hidden');
+   }
+};
 
-burger.addEventListener('click',() => {
-   if (menu.classList.contains('hidden')) {
-		menu.classList.remove('hidden');
-	 } else {
-		menu.classList.add('hidden');
-	 }
-});
+//Container list dropdown
+/*function dropDown() {
+	const list = document.querySelector('#container_list');
 
-containers.addEventListener('click',() => {
-	if(contList.classList.contains('hidden')){
-		contList.classList.remove('hidden');
+	if (list.classList.contains('hidden'))
+	{
+		 list.classList.remove('hidden')
 	} else {
-		contList.classList.add('hidden');
+		 list.classList.add('hidden')
 	}
-});
 
-chevron.addEventListener('click',() => {
-	if(response.classList.contains('hidden')) {
-		response.classList.remove('hidden');
-	} else {
-		response.classList.add('hidden');
-	}
-})
+}; */
+
+
+//Container selection and testimonials slider
+let index = 0;
+
+show_container(index);
+
+function show_container(i){
+   index += i;
+
+   let images = document.getElementsById("#image");
+
+   for(i = 0;i < images.length;i++)
+      images[i].classList.contains("hidden");
+
+   if(index > images.length - 1){
+      index = 0;
+   }
+
+   if(index < 0){
+      index = images.length - 1;
+   }
+      
+
+   images[index].classList.remove("hidden");
+}
+
+//FAQ accordion onClick Action
+
+
+
+// Areas of operation infine carousel
